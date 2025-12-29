3a1380feeeb02f27ea73287b31a294220aceb9ed
pyeondohun <dohoon321@gmail.com>
2025-12-30 00:01:36 +0900

    Add test cases for `article-read` API with support for pagination and infinite scroll.

--- service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
+++ service/article-read/src/test/java/kuke/board/articleread/api/ArticleReadApiTest.java
@@ -1,18 +1,72 @@
 package kuke.board.articleread.api;
 
-import kuke.board.articleread.client.ArticleClient;
+import kuke.board.articleread.service.response.ArticleReadPageResponse;
 import kuke.board.articleread.service.response.ArticleReadResponse;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
+
 public class ArticleReadApiTest {
-    RestClient restClient = RestClient.create("http://localhost:9005");
+    RestClient articleReadRestClient = RestClient.create("http://localhost:9005");
+    RestClient articleRestClient = RestClient.create("http://localhost:9000");
 
     @Test
     void readTest(){
-        ArticleReadResponse body = restClient.get().uri("/v1/articles/{articleId}", 263335757986525184L)
+        ArticleReadResponse body = articleReadRestClient.get().uri("/v1/articles/{articleId}", 263335757986525184L)
                 .retrieve().body(ArticleReadResponse.class);
 
         System.out.println("body = " + body);
+    }
+
+    @Test
+    void readAllTest(){
+        ArticleReadPageResponse response1 = articleReadRestClient.get()
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 1000L, 5))
+                .retrieve()
+                .body(ArticleReadPageResponse.class);
+
+        System.out.println("response1 = " + response1.getArticleCount());
+
+        for (ArticleReadResponse article : response1.getArticles()) {
+
+            System.out.println("article.getArticleId() = " + article.getArticleId());
+        }
+
+        ArticleReadPageResponse response2 = articleReadRestClient.get()
+                .uri("/v1/articles?boardId=%s&page=%s&pageSize=%s".formatted(1L, 1000L, 5))
+                .retrieve()
+                .body(ArticleReadPageResponse.class);
+
+        System.out.println("response2 = " + response2.getArticleCount());
+
+        for (ArticleReadResponse article : response2.getArticles()) {
+
+            System.out.println("article.getArticleId() = " + article.getArticleId());
+        }
+    }
+
+    @Test
+    void readAllInfiniteScrollTest(){
+        List<ArticleReadResponse> response1 = articleReadRestClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L,264043342234914816L))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleReadResponse>>() {
+                });
+
+        for (ArticleReadResponse articleReadResponse : response1) {
+            System.out.println("articleReadResponse.getArticleId() = " + articleReadResponse.getArticleId());
+        }
+
+        List<ArticleReadResponse> response2 = articleReadRestClient.get()
+                .uri("/v1/articles/infinite-scroll?boardId=%s&pageSize=%s&lastArticleId=%s".formatted(1L, 5L,264043342234914816L))
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<ArticleReadResponse>>() {
+                });
+
+        for (ArticleReadResponse articleReadResponse : response2) {
+            System.out.println("articleReadResponse.getArticleId() = " + articleReadResponse.getArticleId());
+        }
     }
 }
